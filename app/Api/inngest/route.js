433865100ca7e60cc4b60b9b69c2d8924386380c
Omkar Awad <omkar236278@gmail.com>
2026-03-25 15:15:29 +0530

    fix import path

--- app/Api/inngest/route.js
+++ app/Api/inngest/route.js
@@ -4,9 +4,8 @@ import {
   syncUserCreation,
   syncUserUpdation,
   syncUserDeletion,
-} from "@/config/inngest";
+} from "@/app/config/inngest";
 
-// Create an API that serves Inngest functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
